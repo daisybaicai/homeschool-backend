af724147680b9b9178342ea39ef4141067a04024
daisybaicai <360619895@qq.com>
2019-01-05 16:44:49 +0800

    noti
    noti

--- src/main/java/com/cdl/demo/controller/NotificationController.java
+++ src/main/java/com/cdl/demo/controller/NotificationController.java
@@ -1,13 +1,13 @@
 package com.cdl.demo.controller;
 
+import com.cdl.demo.domain.Notification;
 import com.cdl.demo.domain.Result;
 import com.cdl.demo.enums.ResultEnum;
 import com.cdl.demo.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/notifications")
@@ -19,6 +19,16 @@ public class NotificationController {
     @GetMapping(value = "", params = {"classId"})
     public Result getNotificationByClassId(int classId) {
         return new Result<>(ResultEnum.SUCCESS, notificationService.getNotificationByClassId(classId));
+    }
+
+    @PostMapping(value = "insertNotification")
+    public Result sendComment(Notification notification, HttpServletRequest request) {
+        int result = notificationService.insertNotification(notification);
+        if (result == 0) {
+            return new Result(ResultEnum.SUCCESS,"成功");
+        } else{
+            return new Result(ResultEnum.ERROR,"错误");
+        }
     }
 
 
--- src/main/java/com/cdl/demo/service/NotificationService.java
+++ src/main/java/com/cdl/demo/service/NotificationService.java
@@ -6,4 +6,6 @@ import java.util.List;
 
 public interface NotificationService {
     List<Notification> getNotificationByClassId(int classId);
+
+    int insertNotification(Notification notification);
 }
